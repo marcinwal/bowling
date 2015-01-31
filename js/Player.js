50b3dc4e2e6d97aa0475e4ba9b637394f7f33e63
marcin walendzik <marcin.walendzik@gmail.com>
2015-01-31 19:16:13 +0000

    throw and player

--- js/Player.js
+++ js/Player.js
@@ -1,7 +1,29 @@
 var player;
 
 
-game(){
-  this.score = 0,
-  this.frames = []
+var Throw = function(){
+  this.pins = 10;
+  this.strike = false;
 }
+
+Throw.prototype.is_strike(pins){
+  if (pins === 10)
+  {
+    this.strike = true;
+  }
+  else
+  {
+    this.srtike = false;
+  }
+}
+
+var Game = function(){
+  this.score = 0;
+  this.frames = [];
+  this.round = 0;
+}
+
+Game.prototype.start(){
+  this.score = 0;
+}
+
